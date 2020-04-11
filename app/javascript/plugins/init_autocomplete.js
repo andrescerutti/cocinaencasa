ca1ee7c339744be0edcce5d54b980fa168357e05
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2020-04-11 20:20:53 -0300

    search only in argentina

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,15 +1,26 @@
 import places from 'places.js';
 
+// var placesAutocomplete = places({
+//     appId: 'pl7E188RO19A',
+//     apiKey: '84aca937548e9ca30c537fbb014687b0',
+//     container:
+//   });
+
 const initAutocomplete = () => {
   const addressInput = document.querySelector('#query_address');
   const addressInput2 = document.querySelector('#order_address_address');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput }).configure({
+    countries: ['ar']
+  });
   }
   if (addressInput2) {
-    places({ container: addressInput2 });
+    places({ container: addressInput2 }).configure({
+    countries: ['ar']
+  });
   }
 };
 
 
 export { initAutocomplete };
+
