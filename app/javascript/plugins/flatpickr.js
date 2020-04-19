3a570abf42a9e7f06afe33315cdd9a9461edec53
andres cerutti <cerutti_andres@hotmail.com>
2020-04-19 19:42:43 -0300

    cambios en calendario

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -7,12 +7,11 @@ require("flatpickr/dist/themes/dark.css");
 
 
   flatpickr(".datepicker", {
-    defaultDate: new Date().fp_incr(3),
+    defaultDate: new Date().fp_incr(0),
     altInput: true,
     altFormat: "D d-M",
-    minDate: new Date().fp_incr(3),
+    minDate: new Date().fp_incr(0),
     maxDate: new Date().fp_incr(10),
-    enableTime: true,
     locale: Spanish,
     disableMobile: "true"
   })
