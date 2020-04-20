897bd43c6dff7aa2f5dbd02c9dfea1d380ca3044
andres cerutti <cerutti_andres@hotmail.com>
2020-04-19 23:33:51 -0300

    cambios del show

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -11,7 +11,7 @@ require("flatpickr/dist/themes/dark.css");
     altInput: true,
     altFormat: "D d-M",
     minDate: new Date().fp_incr(0),
-    maxDate: new Date().fp_incr(10),
+    maxDate: new Date().fp_incr(7),
     locale: Spanish,
     disableMobile: "true"
   })
