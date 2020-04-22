ec20cf00a1f522b84482fdb0102f30acc240089a
andres cerutti <cerutti_andres@hotmail.com>
2020-04-22 20:39:19 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_04_22_180218) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,7 +175,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_180218) do
     t.text "test_mp_private_key_ciphertext"
     t.text "prod_mp_public_key_ciphertext"
     t.text "prod_mp_private_key_ciphertext"
-    t.boolean "own_delivery"
+    t.boolean "own_delivery", default: false
     t.string "day_open"
     t.string "open"
     t.index ["brand_id"], name: "index_restaurants_on_brand_id"
