565e33ec91c2683cd96738894465bf5a548fc628
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2020-04-26 23:02:30 -0300

    couldnt solve step deleting

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
-  has_many :kit_ingredients, inverse_of: :ingredient
+  has_many :kit_ingredients, inverse_of: :ingredient, dependent: :destroy
   accepts_nested_attributes_for :kit_ingredients
   mount_uploader :image, ImageUploader
 
