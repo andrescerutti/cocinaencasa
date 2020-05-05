090564ab2645cc7d389eabc3fa249817e584cfe3
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2020-05-05 01:21:26 -0300

    taked off in mail confirmed order restaurant address

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -2,6 +2,7 @@ class PaymentMailer < ApplicationMailer
   def confirmed
     @user = params[:user]
     @payment = params[:payment]
+    @store = params[:store]
     mail(to: @user, subject: "Tu pago fue aprobado!")
   end
 end
