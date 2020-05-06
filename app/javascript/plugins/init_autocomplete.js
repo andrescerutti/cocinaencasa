b4fddd75dede06c7df61490a2745e900ef653839
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-05-06 02:11:45 -0300

    Update search to Google

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,8 +4,7 @@ function autocomplete() {
     var addressInput2 = document.getElementById('order_address_address');
 
     if (addressInput) {
-      console.log("1")
-      var autocomplete = new google.maps.places.Autocomplete(addressInput, { types: [ 'geocode', 'address'], componentRestrictions: {country: "ar"} });
+      var autocomplete = new google.maps.places.Autocomplete(addressInput, { types: [ 'geocode'], componentRestrictions: {country: "ar"} });
       google.maps.event.addDomListener(addressInput, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault();
@@ -15,7 +14,7 @@ function autocomplete() {
 
     if (addressInput2) {
       console.log("2")
-      var autocomplete = new google.maps.places.Autocomplete(addressInput2, { types: [ 'geocode', 'address'], componentRestrictions: {country: "ar"} });
+      var autocomplete = new google.maps.places.Autocomplete(addressInput2, { types: [ 'geocode' ], componentRestrictions: {country: "ar"} });
       google.maps.event.addDomListener(addressInput2, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault();
