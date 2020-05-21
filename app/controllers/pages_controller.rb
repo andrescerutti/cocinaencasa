ee57f08e5e1d56d22e5cce7c733d5b03f9620708
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-05-20 22:39:29 -0300

    add card btns

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,6 +25,7 @@ class PagesController < ApplicationController
   def admin_dashboard
     delivery_day = Proc.new {|order| order.date_delivery.in_time_zone("Buenos Aires").strftime("%A %d-%b") }
     @restaurant = current_user.restaurants.first
+    @restaurant_kits = Kit.joins(:restaurant).where(restaurant: @restaurant)
     orders = Order.includes(:payment).joins(kit: { restaurant: :user }).where(users: { id: current_user.id }).where(payments: { status: "approved" }).order("orders.date_delivery ASC").where("orders.date_delivery > ?", Date.today.in_time_zone("Buenos Aires") - 1.day )
     @past_orders = Order.includes(:payment).joins(kit: { restaurant: :user }).where(users: { id: current_user.id }).where(payments: { status: "approved" }).order("orders.date_delivery ASC").where("orders.date_delivery <= ?", Date.today.in_time_zone("Buenos Aires") - 1.day ).where("orders.date_delivery > ?", Date.today.in_time_zone("Buenos Aires") - 5.day ).group_by(&delivery_day)
     orders_by_status = orders.group_by { |order| order.status }
